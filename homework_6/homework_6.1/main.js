12017dab47a9a969e78e7a8e110a4def14040b98
Nicolay Vig <nikvigovskij@gmail.com>
2025-01-18 08:26:38 +0200

    homework_6

--- homework_6/homework_6.1/main.js
+++ homework_6/homework_6.1/main.js
@@ -1,1 +1,15 @@
+function removeCharacters(input, charsToRemove) {
+  const charsSet = new Set(charsToRemove);
+  return Array.from(input)
+    .filter((char) => !charsSet.has(char))
+    .join("");
+}
 
+const input = prompt("Введіть рядок:");
+const charsToRemove = prompt(
+  "Введіть символи для видалення, розділені комами:"
+).split(",");
+
+const result = removeCharacters(input, charsToRemove);
+
+alert(`Результат: ${result}`);
