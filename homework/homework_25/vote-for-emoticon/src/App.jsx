2fdc4133de2b9350fdcd45624f2d818c48a5f054
Nicolay Vig <nikvigovskij@gmail.com>
2025-04-08 21:48:59 +0300

    homeWork_26

---
+++ homework/homework_25/vote-for-emoticon/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import EmojiVoting from "./components/EmojiVoting";
+import "./styles.css";
+function App() {
+  return (
+    <div className="App">
+      <EmojiVoting />
+    </div>
+  );
+}
+
+export default App;
